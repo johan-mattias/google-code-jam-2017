53f93e28c9c6fcbbae1429cd06afdf283bd1f753
jsnider <johan.snider@gmail.com>
2017-04-16 09:48:20 +0200

    google code jam practice, got the first problem working on sample input

---
+++ problem1.py
@@ -0,0 +1,60 @@
+import sys
+
+# raw_input() reads a string with a line of input, stripping the '\n' (newline) at the end.
+# This is all you need for most Google Code Jam problems.
+
+debug = 0
+
+t = int(raw_input())  # read a line with a single integer
+for i in xrange(1, t + 1):
+  n, k = raw_input().split(" ")  # read a list of integers, 2 in this case
+
+  flip_count = 0
+  n_list = list(n)
+
+
+  flag = 1
+  imp_flag = 0
+
+  for index in range(len(n_list)):
+    if (n_list[index]!="+"):
+      flag = 0
+      #flip k next spaces
+      if(debug): print "flip"
+      flip_count+=1
+      if(debug): print index, " : ", n_list
+      for x in range(0, int(k)):
+        if((x+index)<len(n_list)):
+          #print "char", n_list[x+index], "index", index, "place", x
+          if(n_list[x+index] == "+"):
+            n_list[x+index] = "-"
+          else:
+            n_list[x+index] = "+"
+        else:
+          imp_flag = 1
+      if(debug): print index, " : ", n_list
+            
+  if flag == 1:
+  	print("Case #{}: {}".format(i, 0))
+  elif imp_flag == 1:
+    print("Case #{}: {}".format(i, "IMPOSSIBLE"))
+  else:
+    print("Case #{}: {}".format(i, flip_count))
+
+
+      # print "1st n", n, ": ", index
+      # flip_count+=1
+      # for j in xrange(0, t):
+      #   if((index+j)<len(n_list)):
+      #     print "hi this many times: ", j
+      #     print "n_list[index+j] ", index+j
+      # if n_list[index+j]=="+":
+      #   n_list[index+j]="-"
+      #   print "bang"
+      # elif n_list[index+j]=="-":
+      #   n_list[index+j]="+"
+      #   print "boom"
+
+
+  #print "Case #{}: {} {}".format(i, n + m, n * m)
+  # check out .format's specification for more formatting options
